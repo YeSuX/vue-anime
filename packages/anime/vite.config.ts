62eb525b3e970e30099ebe70170b86cb33ffd480
YeSuX <351490949@qq.com>
2025-04-15 16:58:48 +0800

    Update @vitejs/plugin-vue to version 5.2.3 in package.json and pnpm-lock.yaml. Enhance vite.config.ts with build configuration for library output, including minification settings, sourcemaps, and Rollup options for external dependencies.

--- packages/anime/vite.config.ts
+++ packages/anime/vite.config.ts
@@ -4,4 +4,33 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    minify: false,
+    sourcemap: true,
+    lib: {
+      entry: './src/index.ts',
+      name: 'anime',
+      fileName: format => `anime.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: [
+        {
+          format: 'es',
+          globals: {
+            vue: 'Vue',
+          },
+          entryFileNames(chunkInfo) {
+            if (chunkInfo.name.includes('node_modules'))
+              return `${chunkInfo.name.replace(/node_modules/g, 'external')}.mjs`
+            return '[name].mjs'
+          },
+          dir: './dist/es',
+          exports: 'named',
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+        },
+      ],
+    },
+  },
 })
