3f9510bf0f33ebfcd3b5bf9174c3b24c8cc7924f
YeSuX <351490949@qq.com>
2025-04-16 23:41:37 +0800

    feat(playground): 新增Vue 3 + TypeScript + Vite示例项目
    在playground目录下创建了一个新的Vue 3 + TypeScript + Vite示例项目，包括基本的项目结构、配置文件和示例组件。此更新为开发者提供了一个快速入门的模板，便于使用Vue 3和TypeScript进行开发，同时集成了Vite作为构建工具。

---
+++ playground/vue/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <Delay />
+</template>
+
+<script setup>
+import Delay from './components/timer/Delay.vue';
+</script>
+
+<style></style>
---
+++ playground/vue/src/components/timer/Delay.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        delay
+    </div>
+</template>
+
+<script setup>
+import { useCreateTimer } from 'vue-anime'
+
+const timer = useCreateTimer({
+    delay: 2000,
+    onUpdate: (self) => {
+        console.log(self);
+    }
+})
+</script>
+
+<style></style>
