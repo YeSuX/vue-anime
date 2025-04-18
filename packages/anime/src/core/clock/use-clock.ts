1cf9ed70a33e0cfa077499661904766afdc98a41
YeSuX <351490949@qq.com>
2025-04-18 10:08:17 +0800

    feat(clock): 重构useClock函数以优化状态管理和时间计算
    在useClock函数中重构状态管理，使用clock对象替代state，增强了fps和speed的getter/setter逻辑。同时，优化了requestTick和computeDeltaTime方法，提升了代码的可读性和可维护性，确保动画帧速率稳定和时间同步功能的实现更加清晰。

--- packages/anime/src/core/clock/use-clock.ts
+++ packages/anime/src/core/clock/use-clock.ts
@@ -3,92 +3,78 @@ import { K, maxFps, minValue, tickModes } from '../consts'
 import { round } from '../utils'
 
 export function useClock(initTime = 0) {
-  const state = reactive({
-    // 当前帧与上一帧之间的时间差，用于动画和物理计算中的时间步长
-    deltaTime: 0,
-    // 当前时间戳，记录当前播放位置或模拟时间
-    _currentTime: initTime,
-    // 从开始到现在已经过去的总时间，用于计算动画进度或总运行时间
-    _elapsedTime: initTime,
-    // 初始化或重置时的时间戳，用作时间计算的基准点
-    _startTime: initTime,
-    // 上一帧的时间戳，用于计算deltaTime
-    _lastTime: initTime,
-    // 当前帧的计划时间，用于帧率控制和时间调度
-    _scheduleTime: 0,
-    // 每一帧的固定时间间隔，用于计算deltaTime
-    _frameDuration: round(K / maxFps, 0),
-    // 每秒帧数，控制动画或模拟的更新频率
-    _fps: maxFps,
-    // 当前播放速度，用于控制动画速度，1.0表示正常速度，<1减速，>1加速
-    _speed: 1,
-    // 是否存在子元素，用于管理嵌套的时间系统
-    _hasChildren: false,
-    // 链表头节点，用于管理多个动画或计时器
-    _head: null,
-    // 链表尾节点，用于管理多个动画或计时器
-    _tail: null,
-  })
+    const clock = reactive({
+        // 当前帧与上一帧之间的时间差，用于动画和物理计算中的时间步长
+        deltaTime: 0,
+        // 当前时间戳，记录当前播放位置或模拟时间
+        _currentTime: initTime,
+        // 从开始到现在已经过去的总时间，用于计算动画进度或总运行时间
+        _elapsedTime: initTime,
+        // 初始化或重置时的时间戳，用作时间计算的基准点
+        _startTime: initTime,
+        // 上一帧的时间戳，用于计算deltaTime
+        _lastTime: initTime,
+        // 当前帧的计划时间，用于帧率控制和时间调度
+        _scheduleTime: 0,
+        // 每一帧的固定时间间隔，用于计算deltaTime
+        _frameDuration: round(K / maxFps, 0),
+        // 每秒帧数，控制动画或模拟的更新频率
+        _fps: maxFps,
+        // 当前播放速度，用于控制动画速度，1.0表示正常速度，<1减速，>1加速
+        _speed: 1,
+        // 是否存在子元素，用于管理嵌套的时间系统
+        _hasChildren: false,
+        // 链表头节点，用于管理多个动画或计时器
+        _head: null,
+        // 链表尾节点，用于管理多个动画或计时器
+        _tail: null,
+        get fps() {
+            return this._fps
+        },
+        set fps(frameRate) {
+            const previousFrameDuration = this._frameDuration
+            const fr = +frameRate
+            const fps = fr < minValue ? minValue : fr
+            const frameDuration = round(K / fps, 0)
+            this._fps = fps
+            this._frameDuration = frameDuration
+            this._scheduleTime += frameDuration - previousFrameDuration
+        },
+        get speed() {
+            return this._speed
+        },
+        set speed(playbackRate) {
+            const pbr = +playbackRate
+            this._speed = pbr < minValue ? minValue : pbr
+        },
+        requestTick(time: number) {
+            // 主要实现了三个关键功能：
+            // 帧速率稳定：保证动画按设定的FPS运行
+            // 跳帧处理：当程序运行缓慢时，会跳过一些帧而不是积压
+            // 时间同步：调整内部时钟，使动画速度与实际时间同步
+            const scheduledTime = this._scheduleTime// 预计的下一帧时间
+            this._elapsedTime = time // 更新经过的时间
 
-  const fps = computed({
-    get: () => state._fps,
-    set: (frameRate) => {
-      const previousFrameDuration = state._frameDuration
-      const fr = +frameRate
-      const fps = fr < minValue ? minValue : fr
-      const frameDuration = round(K / fps, 0)
-      state._fps = fps
-      state._frameDuration = frameDuration
-      state._scheduleTime += frameDuration - previousFrameDuration
-    },
-  })
+            // 如果实际时间还没到预定时间，跳过这一帧
+            if (this._elapsedTime < scheduledTime) {
+                return tickModes.NONE
+            }
 
-  const speed = computed({
-    get: () => state._speed,
-    set: (playbackRate) => {
-      const pbr = +playbackRate
-      state._speed = pbr < minValue ? minValue : pbr
-    },
-  })
+            const frameDuration = this._frameDuration // 每帧应持续的时间
+            const frameDelta = this._elapsedTime - scheduledTime// 实际过去的时间与预计时间的差值
+            // 更新下一帧的预定时间
+            // 确保至少前进一个帧的时长，如果实际过去的时间更多则跳得更远
+            this._scheduleTime += frameDelta < frameDuration ? frameDuration : frameDelta
+            return tickModes.AUTO// 表示应该进行帧更新
+        },
+        // 用于计算两帧之间的时间差
+        computeDeltaTime(time: number) {
+            const delta = time - this._lastTime
+            this.deltaTime = delta
+            this._lastTime = time
+            return delta
+        }
+    })
 
-  // 主要实现了三个关键功能：
-  // 帧速率稳定：保证动画按设定的FPS运行
-  // 跳帧处理：当程序运行缓慢时，会跳过一些帧而不是积压
-  // 时间同步：调整内部时钟，使动画速度与实际时间同步
-  const requestTick = (time: number) => {
-    const scheduledTime = state._scheduleTime// 预计的下一帧时间
-    state._elapsedTime = time // 更新经过的时间
-
-    // 如果实际时间还没到预定时间，跳过这一帧
-    if (state._elapsedTime < scheduledTime) {
-      return tickModes.NONE
-    }
-
-    const frameDuration = state._frameDuration // 每帧应持续的时间
-    const frameDelta = state._elapsedTime - scheduledTime// 实际过去的时间与预计时间的差值
-    // 更新下一帧的预定时间
-    // 确保至少前进一个帧的时长，如果实际过去的时间更多则跳得更远
-    state._scheduleTime += frameDelta < frameDuration ? frameDuration : frameDelta
-    return tickModes.AUTO// 表示应该进行帧更新
-  }
-
-  //   用于计算两帧之间的时间差
-  const computeDeltaTime = (time: number) => {
-    const delta = time - state._lastTime
-    state.deltaTime = delta
-    state._lastTime = time
-    return delta
-  }
-
-  return {
-    // state
-    ...toRefs(state),
-    // computed
-    fps,
-    speed,
-
-    // functions
-    requestTick,
-    computeDeltaTime,
-  }
+    return clock
 }
